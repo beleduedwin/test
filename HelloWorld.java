d96dcfea8424c0fd7fb564598109d0096aa61717
lapiscine <beleduedwin@yahoo.fr>
2019-07-03 16:30:46 +0200

    test repo

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+class HelloWorld {
+//test
+	//String[] args : on passe le type et sa variable
+    public static void main(String[] args) {
+        // afficher Hello World! dans le terminal
+        System.out.println("Hello World!");
+        
+    }
+
+
+}
